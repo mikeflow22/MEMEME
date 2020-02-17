14630ce694aba600338c383fec4522ef45be3285
Michael R.F <mrflowers22@uwalumni.com>
2020-02-17 10:51:45 -0800

    created  a model struct name meme

---
+++ MEMEME/Model/Meme.swift
@@ -0,0 +1,15 @@
+//
+//  Meme.swift
+//  MEMEME
+//
+//  Created by Michael Flowers on 2/17/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    let image: UIImage
+    let topText: String
+    let bottomText: String
+}
