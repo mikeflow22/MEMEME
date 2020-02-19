d3fcd20843bf1703b66804421194c54ba34b6908
Michael R.F <mrflowers22@uwalumni.com>
2020-02-18 21:01:04 -0800

    implemented functionality to update tableViewCell for meme

--- MEMEME/Views/MemeCellTableViewCell.swift
+++ MEMEME/Views/MemeCellTableViewCell.swift
@@ -9,5 +9,21 @@
 import UIKit
 
 class MemeCellTableViewCell: UITableViewCell {
-
+    var meme: Meme? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var topTextField: UILabel!
+     
+    private func updateViews(){
+        guard let passedInMeme = meme else {
+            print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+            return
+        }
+        memeImageView.image = passedInMeme.memeImage
+        topTextField.text = passedInMeme.topText
+    }
 }
