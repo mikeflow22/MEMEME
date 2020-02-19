f668b5b635b1efcd69a3380d208eaf4732464958
Michael R.F <mrflowers22@uwalumni.com>
2020-02-19 07:44:55 -0800

    added MemeCollectionViewCell and assigned it to the cell in storyboard.

---
+++ MEMEME/Storyboard/MemeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemeCollectionViewCell.swift
+//  MEMEME
+//
+//  Created by Michael Flowers on 2/19/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+}
