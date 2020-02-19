86b893ad8c813bdba71d7b13e61a9ecd3cc63ae5
Michael R.F <mrflowers22@uwalumni.com>
2020-02-18 20:45:31 -0800

    added MemeCellTableViewCell file and assigned it to the storyboard scene.

--- MEMEME/Storyboard/SentMemesTableViewController.swift
+++ MEMEME/Storyboard/SentMemesTableViewController.swift
@@ -27,15 +27,6 @@ class SentMemesTableViewController: UITableViewController {
         
         return cell
     }
-   
-
-   
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    
 
     /*
     // Override to support editing the table view.
@@ -48,22 +39,6 @@ class SentMemesTableViewController: UITableViewController {
         }    
     }
     */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     /*
     // MARK: - Navigation
 
---
+++ MEMEME/Views/MemeCellTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCellTableViewCell.swift
+//  MEMEME
+//
+//  Created by Michael Flowers on 2/18/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class MemeCellTableViewCell: UITableViewCell {
+
+}
